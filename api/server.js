adaa083cc3d9ae70d120fe328ffa75e8ffb8ad01
corey-power <CPower1248@gmail.com>
2021-01-22 14:40:16 -0500

    Server listening

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here
+const express = require("express")
+const server = express()
+server.use(express.json())
+
+const projectsRouter = require("./project/router")
+const resourcesRouter = require("./resource/router")
+const tasksRouter = require("./task/router")
+
+server.use("/api/projects", projectsRouter)
+server.use("/api/resources", resourcesRouter)
+server.use("/api/tasks", tasksRouter)
+
+module.exports = server
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,6 @@
-// build your `/api/tasks` router here
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res, next) => {})
+
+module.exports = router
