f86242dfbd928db5c647300b39e701bbfa814673
corey-power <CPower1248@gmail.com>
2021-01-23 20:38:58 -0500

    [FIX] Endpoint returns response correctly

--- api/project_resources/model.js
+++ api/project_resources/model.js
@@ -10,6 +10,7 @@ function findAll() {
   let query = db("project_resources as pr")
     .leftJoin("projects as p", "pr.project_id", "p.project_id")
     .leftJoin("resources as r", "pr.resource_id", "r.resource_id")
+    .orderBy("p.project_id")
   
   return query.then(project_resources => {
     return project_resources.map(project_resource => helpers.projectToBody(project_resource))
@@ -19,7 +20,10 @@ function findAll() {
 function insert(body) {
   let query = db("project_resources").insert(body)
     .then(([id]) => {
-      return db("project_resources").where({ project_resource_id: id }).first()
+      return db("project_resources as pr")
+      .leftJoin("projects as p", "pr.project_id", "p.project_id")
+      .leftJoin("resources as r", "pr.resource_id", "r.resource_id")
+      .where("pr.project_resource_id", id).first()
     })
 
   return query.then(newProjectResource => helpers.project_resourceToBody(newProjectResource))
